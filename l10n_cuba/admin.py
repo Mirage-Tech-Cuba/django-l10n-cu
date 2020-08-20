b67696155acfa743ab7ca638530ecf1e0a98a4cb
jorge.alfonso <jorge.alfonso@mtz.desoft.cu>
2020-08-20 15:43:57 -0400

    [ADD] feature to enable/disable admin menu

--- l10n_cuba/admin.py
+++ l10n_cuba/admin.py
@@ -1,27 +1,30 @@
+from django.conf import settings
 from django.contrib import admin
 
-from .models import Provincia, Municipio, CodigoPostal
+L10N_CUBA_ADMIN = getattr(settings, "L10N_CUBA_ADMIN", False)
 
-# Register your models here.
-admin.site.register(Provincia)
+if L10N_CUBA_ADMIN:
 
+    from .models import Provincia, Municipio, CodigoPostal
 
-@admin.register(Municipio)
-class MunicipioAdmin(admin.ModelAdmin):
-    list_filter = ['provincia']
-    list_display = ('provincia', 'nombre', 'codigos')
-    empty_value_display = '---'
+    # Register your models here.
+    admin.site.register(Provincia)
 
-    def codigos(self, obj):
-        return ' - '.join(str(codigo.full_value) for codigo in obj.codigos_postales.all())
+    @admin.register(Municipio)
+    class MunicipioAdmin(admin.ModelAdmin):
+        list_filter = ['provincia']
+        list_display = ('provincia', 'nombre', 'codigos')
+        empty_value_display = '---'
 
+        def codigos(self, obj):
+            return ' - '.join(str(codigo.full_value) for codigo in obj.codigos_postales.all())
 
-@admin.register(CodigoPostal)
-class CodigoPostalAdmin(admin.ModelAdmin):
-    list_filter = ['municipio__provincia']
-    list_display = ('provincia', 'municipio', 'codigo_postal', 'localidad')
-    ordering = ['municipio', 'codigo_postal']
-    empty_value_display = '---'
+    @admin.register(CodigoPostal)
+    class CodigoPostalAdmin(admin.ModelAdmin):
+        list_filter = ['municipio__provincia']
+        list_display = ('provincia', 'municipio', 'codigo_postal', 'localidad')
+        ordering = ['municipio', 'codigo_postal']
+        empty_value_display = '---'
 
-    def provincia(self, obj):
-        return obj.municipio.provincia
+        def provincia(self, obj):
+            return obj.municipio.provincia
